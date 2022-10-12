f546f2908ae5c5cc5120162eab9fb49532ee1fb6
Darshit <darshitmistry2@gmail.com>
2022-10-12 22:14:29 +0530

    cardDeal section was added priviously

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import 
 {apple,bill,google} from '../assets'
-import styles ,{layout} from '../style'
+import styles , { layout } from '../style'
 
 const Billing = () => (
   <section id='product' className={layout.sectionReverse}>
@@ -15,7 +15,7 @@ const Billing = () => (
   <h2>
     Easily control your <br className='sm:block hidden'/> billing & invoicing.
   </h2>
-  <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Elit enim sed ,assa etim. Mauris eu asdipising ultrices ametodio aeneaneeque. Fusce ipsum oric rhonus aliporttitor integer platea placerat.</p>
+  <p className={` ${styles.paragraph} max-w-[470px] mt-5`}>Elit enim sed ,assa etim. Mauris eu asdipising ultrices ametodio aeneaneeque. Fusce ipsum oric rhonus aliporttitor integer platea placerat.</p>
   <div className='flex flex-row flex-wrap am:mt-10 mt-6'>
     <img src={apple} alt="" className='w-[128px] h-42px object-contain mr-5 cursor-pointer'/>
     <img src={google} alt="" className='w-[128px] h-42px object-contain  cursor-pointer'/>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import styles from '../style'
 
 const Button = ({style}) => {
   return (
-    <button type="button" className={`py-4 px-6 bg-blue-gredient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
+    <button type="button" className={`py-4 px-6 bg-blue-gredient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}>
       Get Started
     </button>
    )
