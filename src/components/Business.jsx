4050343c32420008508b9c808822cbeb255f9d05
Darshit <darshitmistry2@gmail.com>
2022-10-12 21:50:08 +0530

    Adding a Billing part and solve some additional issues in Business component

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -2,20 +2,23 @@ import React from 'react'
 import { features } from '../constants'
 import styles, {layout} from '../style'
 import Button from './Button'
-const featureCard =({icon,title,content,index})=>(
-  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length -1 ? "mb-6" : "mb-0"} feature-card`}>
-    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
-      <img src={icon} alt="icon" className='w-[50%] h-[50%]'/>
-    </div>
-    <div className="flex-1 flex-col flex ml-3">
-      <h4 className='font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1'>
-        {title}
-      </h4>
-      <p className='font-poppins font-normal text-dimWhite text-[18px] leading-[23px] mb-1'>{content}</p>
-    </div>
 
-  </div>
-)
+ const FeatureCard = ({ icon, title, content, index }) => {
+  return (
+    <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
+      <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+        <img src={icon} alt="icon" className='w-[50%] h-[50%]' />
+      </div>
+      <div className="flex-1 flex-col flex ml-3">
+        <h4 className='font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1'>
+          {title}
+        </h4>
+        <p className='font-poppins font-normal text-dimWhite text-[18px] leading-[23px] mb-1'>{content}</p>
+      </div>
+
+    </div>
+  )
+}
 
 
 const Business= () =>{
@@ -29,7 +32,7 @@ const Business= () =>{
           </div>
           <div className={`${layout.sectionImg}`}>
             {features.map((feature, index)=>(
-              <featureCard key={feature.id}{...feature} index={index}/>
+              <FeatureCard key={feature.id}{...feature} index={index}/>
             ))}
           </div>
           </section>
