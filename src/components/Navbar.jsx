4376e7268d524d2b2dfa56c5d74bc90b9eb5e2ce
Darshit <darshitmistry2@gmail.com>
2022-10-12 13:36:55 +0530

    Updating Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import {useState} from 'react'
 import { close, logo, menu} from '../assets';
 import {navLinks} from '../constants';
+
 const Navbar = () => {
   const[toggle,setToggle] = useState(false)
   return (
    <nav className= "w-full flex py-6 justify-between items-center navbar" >
-      <img src={logo} alt="hookbank"className='w-[124px] h-[32px]'
+      <img src={logo} alt="hoobank"className='w-[124px] h-[32px]'
        />
        <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav ,index) =>(
           <li
-          key={nav.id} className={`font-poppins font-normal cursor-pointer ${index ===navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-[16px] text-white`}>
+          key={nav.id} className={`font-poppins font-normal cursor-pointer ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-[16px] text-white`}>
             <a href={`#${nav.id}`}>
               {nav.title}
             </a>
@@ -18,14 +19,14 @@ const Navbar = () => {
         ))}
 
        </ul>
-       <div className="sm:hidden flex flex-row flex-1 justify-end item-center">
+       <div className="sm:hidden flex flex-row flex-1 justify-end items-center">
           <img src={toggle ? close : menu} alt="menu" className='w-[28px h-[28px] object-contain' 
           onClick={()=> setToggle((prev) => !prev)}/>
           <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] rounded-xl sidebar`}>
           <ul className="list-none flex flex-col justify-end items-center flex-1">
         {navLinks.map((nav ,index) =>(
           <li
-          key={nav.id} className={`font-poppins font-normal cursor-pointer ${index ===navLinks.length - 1 ? 'mr-0' : 'mb-14'} text-[16px] text-white`}>
+          key={nav.id} className={`font-poppins font-normal cursor-pointer ${index === navLinks.length - 1 ? 'mr-0' : 'mb-14'} text-[16px] text-white`}>
             <a href={`#${nav.id}`}>
               {nav.title}
             </a>
