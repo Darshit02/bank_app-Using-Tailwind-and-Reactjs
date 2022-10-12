b19b24a986b9a912f900fecfaf3770015ea67873
Darshit <darshitmistry2@gmail.com>
2022-10-12 22:57:33 +0530

    Usually added a testimonials component in hoobanl

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import { clients } from "../constants"
+import styles from "../style"
 
-const Clients = () => {
-  return (
-    <div>
-      Clients
+const Clients = () => (
+  <section className={`${styles.flexCenter} mx-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div  key={client.id} className={` flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
+          <img src={client.logo} alt="" className="sm:w-[192px] w-[100px] object-contain"/>
+        </div>
+      ))}
     </div>
-  )
-}
+  </section>
+)
 
 export default Clients
